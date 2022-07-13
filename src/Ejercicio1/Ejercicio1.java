3d5535148bd2cf585963dc73912f7d5b7b21c571
Jhaldreen <antjimenez1@gmail.com>
2022-07-13 12:38:07 +0200

    ejercicio 1

--- src/Ejercicio1/Ejercicio1.java
+++ src/Ejercicio1/Ejercicio1.java
@@ -1,2 +1,32 @@
-package Ejercicio1;public class Ejercicio1 {
+package Ejercicio1;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        // 1. numericos
+
+        // 1.1 enteros
+        byte var1 = 8;
+        short var2 = 9;
+        int var3 = 10;
+        long var4 = 300;
+        long numPhone = 666789123;
+
+        //1 .2 decimales
+        float var5 = 3.3f;
+        double var6 = 14.6d;
+        var6 =28.9d;
+        double var7,var8 = 34.6d;
+
+        // 2 . booleanos
+
+        boolean var9 = true;
+        boolean var10 = false;
+
+        //3 . texto
+
+        char varTexto1 = 'a';
+        String varTexto2 = "Hola soy un String";
+    }
 }
